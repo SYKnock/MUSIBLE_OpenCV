bb432cc5ff6a76bf80e8a291d0e17701a867fede
SonYongLak <knockrock20@naver.com>
2021-05-17 02:10:00 +0900

    multiple input function

--- main.cpp
+++ main.cpp
@@ -3,37 +3,50 @@
 int main(int argc, char *argv[])
 {
     // error if there is no input file path
-    if (argc != 2)
+    if (argc < 2)
         return 0;
 
-    // Mat score = imread("images.jpeg", IMREAD_GRAYSCALE);
-    Mat firstScore = imread(argv[1], IMREAD_GRAYSCALE);
+    int cnt = argc - 1;
 
-    // error if program fail to read image
-    if (firstScore.empty())
-        return 0;
+    for (int i = 1; i <= cnt; i++)
+    {
+        xLineCnt = 1;
+        yLineCnt = 1;
+        realX = 1;
+        
 
-    Mat score;
+        // Mat score = imread("images.jpeg", IMREAD_GRAYSCALE);
+        Mat firstScore = imread(argv[i], IMREAD_GRAYSCALE);
 
-    int resizeRow = firstScore.rows * 4 / 5;
-    int resizeCols = firstScore.cols * 4 / 5;
+        // error if program fail to read image
+        if (firstScore.empty())
+            return 0;
 
-    resize(firstScore, score, Size(resizeCols, resizeRow));
+        Mat score;
 
-    // namedWindow("score", 0);
-    // imshow("score", score);
-    // resizeWindow("score", resizeCols, resizeRow);
+        int resizeRow = firstScore.rows * 4 / 5;
+        int resizeCols = firstScore.cols * 4 / 5;
 
-    Mat normalScore = preprocess(score);
+        resize(firstScore, score, Size(resizeCols, resizeRow));
 
-    // namedWindow("Result1", 0);
-    // imshow("Result1", normalScore);
-    // resizeWindow("Result1", resizeCols, resizeRow);
+        // namedWindow("score", 0);
+        // imshow("score", score);
+        // resizeWindow("score", resizeCols, resizeRow);
 
-    Mat BrailleScore(score.size(), CV_8UC3);
-    BrailleScore = findCircle(normalScore);
+        Mat normalScore = preprocess(score);
 
-    convert2Score();
+        // namedWindow("Result1", 0);
+        // imshow("Result1", normalScore);
+        // resizeWindow("Result1", resizeCols, resizeRow);
+
+        Mat BrailleScore(score.size(), CV_8UC3);
+        BrailleScore = findCircle(normalScore);
+
+        convert2Score();
+
+
+        brailleSet.clear();
+    }
 
     for (int i = 0; i < noteSet.size(); i++)
     {
